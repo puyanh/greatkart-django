e9b28ff92f2985c24bc237f856109b53f12fbe87
puyanh <puyanh18@gmail.com>
2021-04-30 22:08:55 -0600

    add cart function, add, delete

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
@@ -24,6 +26,7 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
@@ -43,7 +46,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        # 'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         # 'orderproduct': orderproduct,
         # 'reviews': reviews,
         # 'product_gallery': product_gallery,
