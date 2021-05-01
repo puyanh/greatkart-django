26a0f1f829a48b221a3e5706cc045de8d157a0ba
puyanh <66757514+puyanh@users.noreply.github.com>
2021-05-01 16:23:25 -0600

    delete personal info

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -144,7 +144,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'codingTest21@gmail.com'
-EMAIL_HOST_PASSWORD = 'Stayhealthy1!'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # EMAIL_HOST = config('EMAIL_HOST')
