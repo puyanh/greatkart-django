c53f57ef28dfbb73c5fda423aae0e9153b6e0ff1
puyanh <puyanh18@gmail.com>
2021-05-01 16:13:35 -0600

    added register, login, email verification, logout features

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,21 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'codingTest21@gmail.com'
+EMAIL_HOST_PASSWORD = 'Stayhealthy1!'
+EMAIL_USE_TLS = True
+
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-
+    path('accounts/', include('accounts.urls')),
     # ORDERS
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
