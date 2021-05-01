1fdc551fbd77d075cad3f12090741b5d730b3be8
puyanh <puyanh18@gmail.com>
2021-05-01 12:21:32 -0600

    added productions variation features

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 # from accounts.models import Account
 
 # Create your models here.
@@ -13,7 +13,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     # user = models.ForeignKey(Account, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # variations = models.ManyToManyField(Variation, blank=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -21,7 +21,7 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
-    # def __unicode__(self):
-    #     return self.product
-    def __str__(self):
+    def __unicode__(self):
         return self.product
+    # def __str__(self):
+    #     return self.product
